2f04dd09321dd01155b0c740d13ed42f392ad3e8
Plenett <31214455+Plenett@users.noreply.github.com>
2021-03-26 09:20:37 +0000

    gitignore + options with clap

--- cipher/src/main.rs
+++ cipher/src/main.rs
@@ -1,17 +1,36 @@
 use clap::Clap;
 
 #[derive(Clap)]
-#[clap(version = "0.1", author = "Théo P.")]
+#[clap(name = "cipher", about = "Command line application that cipher some message.", version = "0.1", author = "Théo P.")]
 struct Opts {
-    /// A level of verbosity, and can be used multiple times
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: i32,
-		/// Some input. Because this isn't an Option<T> it's required to be used
-    input: i32,
+    /// Vigenère Cipher, need a <String> key
+    #[clap(short, long)]
+    vigenere: bool,
+		/// Caesar Cipher, need a <u8> key
+    #[clap(short, long)]
+    caesar: bool,
+		/// Decipher <message> with the cipher use and the <key>
+    #[clap(short, long)]
+    decipher: bool,
+		/// Encryption Key
+    key: String,
+		/// Message to cipher
+    message: String,
 }
 
 fn main() {
 	let opts: Opts = Opts::parse();
 
-	println!("input = {}", opts.input);
+	println!("input = {}", opts.message);
+	println!("isVigenere = {}", opts.vigenere);
+	println!("Key = {}", opts.key);
+	
+}
+
+fn vigenere_cipher(message : String, key : String, decipher : bool) -> String{
+
+}
+
+fn caesar_cipher(message : String, key : u8, decipher : bool) -> String{
+
 }
