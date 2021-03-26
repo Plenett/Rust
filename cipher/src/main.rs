8ff4fa0af9096c43d07d2b57d10b24fb88e06f80
Plenett <31214455+Plenett@users.noreply.github.com>
2021-03-26 10:07:50 +0000

    cipher main done

--- cipher/src/main.rs
+++ cipher/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Clap;
 
+/* Options definition*/
 #[derive(Clap)]
 #[clap(name = "cipher", about = "Command line application that cipher some message.", version = "0.1", author = "Théo P.")]
 struct Opts {
@@ -21,16 +22,36 @@ struct Opts {
 fn main() {
 	let opts: Opts = Opts::parse();
 
-	println!("input = {}", opts.message);
-	println!("isVigenere = {}", opts.vigenere);
-	println!("Key = {}", opts.key);
+	if opts.vigenere && opts.caesar {
+		eprintln!("error: several cipher error")
+	}
+
+	if opts.vigenere {
+		let message = vigenere_cipher(opts.message, opts.key, opts.decipher);
+		println!("{}", message);
+	}else if opts.caesar {
+		let key : u8 = match u8::from_str_radix(&opts.key, 10) {
+				Ok(val) => val,
+				Err(err) => std::process::exit({
+            eprintln!("error: {:?}", err);
+            0
+        }),
+		};
+
+		let message = caesar_cipher(opts.message, key, opts.decipher);
+		println!("{}", message);
+	}else {
+		eprintln!("error: cipher type error");
+	}
 	
 }
 
 fn vigenere_cipher(message : String, key : String, decipher : bool) -> String{
-
+	let message = String::new();
+	return message;
 }
 
 fn caesar_cipher(message : String, key : u8, decipher : bool) -> String{
-
+	let message = String::new();
+	return message;
 }
